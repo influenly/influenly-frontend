a3492834a5a99d6989f4ee2ecb0870702e7c96e6
leandro.ibaceta <leandro_ibaceta@tecso.coop>
2023-08-13 21:56:59 -0300

    Create networks component in Profile
    Add styling and mock data to network section
    Add margin -3rem to container-box because 100vh increase height with HeaderComponent in top

--- src/app/main/pages/profile/network-basic-info/network-basic-info.component.ts
+++ src/app/main/pages/profile/network-basic-info/network-basic-info.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-network-basic-info',
   templateUrl: './network-basic-info.component.html',
   styleUrls: ['./network-basic-info.component.scss']
 })
-export class NetworkBasicInfoComponent {
+export class NetworkBasicInfoComponent implements OnInit {
 
+  youtube = {
+    subs: 88756,
+    videos: 106,
+    visits: 165590,
+    likes: 14454
+  }
+
+  data : any = {};
+
+  ngOnInit() {
+    this.data.subs = this.transformNumbers(this.youtube.subs);
+    this.data.videos = this.transformNumbers(this.youtube.videos);
+    this.data.visits = this.transformNumbers(this.youtube.visits);
+    this.data.likes = this.transformNumbers(this.youtube.likes);
+  }
+
+  transformNumbers(number: number): string {
+    return number > 1000 ? Math.floor(number / 1000) + 'K' : number + '';
+  }
 }
