3a1870460d4eb2d197d849be80b3a92cd833b3b5
leandro.ibaceta <leandro_ibaceta@tecso.coop>
2023-09-13 12:36:41 -0300

    Mark selected conversation

--- src/app/main/pages/chat-window/talks/talks.component.ts
+++ src/app/main/pages/chat-window/talks/talks.component.ts
@@ -11,7 +11,10 @@ export class TalksComponent {
   @Input() conversations: { pending: ConversationModel[], inProgress: ConversationModel[], finished: ConversationModel[] }|undefined;
   @Output() selectedConversation: EventEmitter<ConversationModel> = new EventEmitter();
 
+  selectedConversationId: number = 0;
+
   selectConversation(conversation: ConversationModel) {
+    this.selectedConversationId = conversation.id;
     this.selectedConversation.emit(conversation);
   }
 
