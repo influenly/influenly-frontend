294df2a815429f56dc4bb870a670785dd0a9455b
Leandro Ibaceta <leandro_ibaceta@hotmail.com>
2024-05-06 21:13:39 -0300

    Integrate networks on profile

--- src/app/main/pages/profile/network-basic-info/network-basic-info.component.ts
+++ src/app/main/pages/profile/network-basic-info/network-basic-info.component.ts
@@ -3,6 +3,14 @@ import { NetworkProfileModel, UserModel } from '../models/user-data.model';
 import { ProfileService } from '../services/profile.service';
 import { Platform } from 'src/app/shared/constants/platforms.enum';
 import { NetworkSelectorComponent } from '../network-selector/network-selector.component';
+import { IntegrationModel } from '../../onboarding/models/integration.model';
+import { OnboardingService } from '../../onboarding/services/onboarding.service';
+import { InformationModalComponent } from 'src/app/shared/components/UI/information-modal/information-modal.component';
+import { MatDialog } from '@angular/material/dialog';
+import { TranslateService } from '@ngx-translate/core';
+
+declare var initTokenClient: Function;
+declare var getToken: Function;
 
 @Component({
   selector: 'app-network-basic-info',
@@ -19,7 +27,12 @@ export class NetworkBasicInfoComponent implements OnInit, OnChanges {
   selectedNetwork: { platform: string, networks: NetworkProfileModel[] } | undefined;
   data: any[] = [];
 
-  constructor(private profileService: ProfileService) { }
+  constructor(
+    private profileService: ProfileService,
+    private onboardingService: OnboardingService,
+    private dialog: MatDialog,
+    private translate: TranslateService
+  ) { }
 
   ngOnInit() {
     this.setTransformedData();
@@ -77,6 +90,49 @@ export class NetworkBasicInfoComponent implements OnInit, OnChanges {
 
   openNetworkPage(network: NetworkProfileModel) {
     window.open(network.url?.includes('https://') ? network.url : 'https://' + network.url, '_blank');
+  }
+
+  async integrateNetwork(network: NetworkProfileModel) {
+    await initTokenClient(async (response: any) => {
+      const payload: IntegrationModel = {
+        authorizationCode : response.code,
+        platform: Platform.Youtube
+      }
+      this.onboardingService.integration$(payload).subscribe({
+        next: (v) => {
+          this.dialog.open(InformationModalComponent, {
+            width: '600px',
+            data: {
+              icon: 'check',
+              title: this.translate.instant('profile.networks.integration.ok'),
+              textButtonClose: this.translate.instant('general.btn_return')
+            }
+          });
+        },
+        error: (e) => {
+          if (e.error?.error === 'Network already integrated') {
+            this.dialog.open(InformationModalComponent, {
+              width: '600px',
+              data: {
+                icon: 'warning',
+                title: this.translate.instant('profile.networks.integration.already_integrated'),
+                textButtonClose: this.translate.instant('general.btn_return')
+              }
+            });
+          } else {
+            this.dialog.open(InformationModalComponent, {
+              width: '600px',
+              data: {
+                icon: 'warning',
+                title: this.translate.instant('profile.networks.integration.error'),
+                textButtonClose: this.translate.instant('general.btn_return')
+              }
+            });
+          }
+        }
+      });
+    });
+    getToken();
   }
 
 }
